da243a9fa1807ca53eb15043a88ae5ccca4cbd4c
wqdotnet <110328722@qq.com>
2019-12-23 17:54:23 +0800

    first commit

---
+++ cmd/root.go
@@ -0,0 +1,99 @@
+/*
+Copyright © 2019 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+// ServerConfig  server cfg
+type ServerConfig struct {
+	CfgFile string
+	Host    string
+}
+
+// ServerCfg  Program overall configuration
+var ServerCfg = ServerConfig{
+	CfgFile: "",
+	Host:    "localhost:8080",
+}
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "demo",
+	Short: "Short",
+	Long:  `long`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	//initConfig()
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&ServerCfg.CfgFile, "config", "", "config file (default is $HOME/demo.yaml)")
+	rootCmd.PersistentFlags().StringVar(&ServerCfg.Host, "host", "", "localhost:8080")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	fmt.Println("cfgFile:", ServerCfg.CfgFile)
+	fmt.Println("ServerCfg cfgFile:", ServerCfg.CfgFile)
+	fmt.Println("ServerCfg host:", ServerCfg.Host)
+
+	if ServerCfg.CfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(ServerCfg.CfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		// Search config in home directory with name ".demo" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName("demo")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module server
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/prometheus/common v0.4.0
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/viper v1.6.1
+)
