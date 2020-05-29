b18a96fd64594284255d5fcdba6e508faa193ef8
wqdotnet <110328722@qq.com>
2020-05-29 17:07:30 +0800

    网络库

--- go.mod
+++ go.mod
@@ -3,12 +3,15 @@ module server
 go 1.13
 
 require (
+	github.com/davyxu/cellnet v4.1.0+incompatible
+	github.com/davyxu/goobjfmt v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.5.0
 	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/golang/protobuf v1.3.2
 	github.com/klauspost/cpuid v1.2.2 // indirect
 	github.com/klauspost/reedsolomon v1.9.3 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/panjf2000/gnet v1.2.0
 	github.com/prometheus/common v0.4.0
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.6.1
--- network/tcp.go
+++ network/tcp.go
@@ -1,11 +1,51 @@
 package network
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net"
-	"time"
+
+	"github.com/panjf2000/gnet"
 )
+
+type echoServer struct {
+	*gnet.EventServer
+}
+
+func (es *echoServer) OnInitComplete(srv gnet.Server) (action gnet.Action) {
+	log.Printf("Echo server is listening on %s (multi-cores: %t, loops: %d)\n",
+		srv.Addr.String(), srv.Multicore, srv.NumEventLoop)
+	return
+}
+func (es *echoServer) React(frame []byte, c gnet.Conn) (out []byte, action gnet.Action) {
+	// Echo synchronously.
+	out = frame
+	return
+
+	/*
+		// Echo asynchronously.
+		data := append([]byte{}, frame...)
+		go func() {
+			time.Sleep(time.Second)
+			c.AsyncWrite(data)
+		}()
+		return
+	*/
+}
+
+func main() {
+	var port int
+	var multicore bool
+
+	// Example command: go run echo.go --port 9000 --multicore=true
+	flag.IntVar(&port, "port", 9000, "--port 9000")
+	flag.BoolVar(&multicore, "multicore", false, "--multicore true")
+	flag.Parse()
+	echo := new(echoServer)
+	log.Fatal(gnet.Serve(echo, fmt.Sprintf("tcp://:%d", port), gnet.WithMulticore(multicore)))
+}
 
 //TCPNetwork tcp/ip
 type TCPNetwork struct {
@@ -15,7 +55,10 @@ type TCPNetwork struct {
 
 //Start NetworkInterface.Start
 func (c TCPNetwork) Start() {
-	fmt.Printf("TcpNetwork start")
+	fmt.Println("TcpNetwork start")
+
+	// gnet.Serve(c, fmt.Sprintf("tcp://:%d", "port"), gnet.WithMulticore(true))
+
 	tcpServer, _ := net.ResolveTCPAddr("tcp4", ":8080")
 	listener, _ := net.ListenTCP("tcp", tcpServer)
 
@@ -33,18 +76,19 @@ func (c TCPNetwork) Start() {
 }
 
 func handle(conn net.Conn) {
-	defer conn.Close()
-
+	//defer conn.Close()
+	fmt.Println("socket handle")
 	//读取客户端传送的消息
 	go func() {
+
 		response, _ := ioutil.ReadAll(conn)
 		fmt.Println(string(response))
 	}()
 
-	//向客户端发送消息
-	time.Sleep(1 * time.Second)
-	now := time.Now().String()
-	conn.Write([]byte(now))
+	// //向客户端发送消息
+	// time.Sleep(1 * time.Second)
+	// now := time.Now().String()
+	// conn.Write([]byte(now))
 }
 
 //Stop NetworkInterface.Stop
