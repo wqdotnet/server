46034bb3857964c675ae3705c363593c0325b2ec
unknown <wqdotnet>
2020-06-03 22:24:48 +0800

    socket test code

--- go.mod
+++ go.mod
@@ -5,20 +5,17 @@ go 1.13
 require (
 	github.com/davyxu/cellnet v4.1.0+incompatible
 	github.com/davyxu/goobjfmt v0.1.0 // indirect
+	github.com/fwhezfwhez/errorx v0.0.0-20200106103634-5bceb7ccfe70
+	github.com/fwhezfwhez/tcpx v0.0.0-20200110044634-c35f3f515f4e
 	github.com/gin-gonic/gin v1.5.0
 	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/golang/protobuf v1.3.2
-	github.com/klauspost/cpuid v1.2.2 // indirect
-	github.com/klauspost/reedsolomon v1.9.3 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/panjf2000/gnet v1.2.0
 	github.com/prometheus/common v0.4.0
 	github.com/spf13/cobra v0.0.5
 	github.com/spf13/viper v1.6.1
-	github.com/templexxx/cpufeat v0.0.0-20180724012125-cef66df7f161 // indirect
-	github.com/templexxx/xor v0.0.0-20191217153810-f85b25db303b // indirect
-	github.com/tjfoc/gmsm v1.2.0 // indirect
 	github.com/xtaci/kcp-go v5.4.20+incompatible
 	github.com/xtaci/lossyconn v0.0.0-20190602105132-8df528c0c9ae // indirect
-	golang.org/x/crypto v0.0.0-20191219195013-becbf705a915
+	golang.org/x/crypto v0.0.0-20191227163750-53104e6ec876
 )
--- network/tcp.go
+++ network/tcp.go
@@ -1,9 +1,15 @@
 package network
 
 import (
+	"bytes"
+	"encoding/binary"
 	"errors"
 	"fmt"
+	"io"
 	"net"
+	"os"
+
+	"github.com/fwhezfwhez/errorx"
 )
 
 //TCPNetwork tcp/ip
@@ -25,50 +31,112 @@ func (in *innerBuffer) readN(n int) (buf []byte, err error) {
 	return
 }
 
-//Start NetworkInterface.Start
-func (c *TCPNetwork) Start() {
-	fmt.Println("TcpNetwork start")
-	tcpServer, _ := net.ResolveTCPAddr("tcp4", ":8080")
-	listener, _ := net.ListenTCP("tcp", tcpServer)
+func bytesToInt(bys []byte) int {
+	bytebuff := bytes.NewBuffer(bys)
+	var data int64
+	binary.Read(bytebuff, binary.LittleEndian, &data)
+	return int(data)
+}
 
+func readUntil(reader io.Reader, buf []byte) error {
+	if len(buf) == 0 {
+		return nil
+	}
+	var offset int
 	for {
-		//当有新的客户端请求来的时候，拿到与客户端的连接
-		conn, err := listener.Accept()
+		n, e := reader.Read(buf[offset:])
+		if e != nil {
+			if e == io.EOF {
+				return e
+			}
+			return errorx.Wrap(e)
+		}
+		offset += n
+		if offset >= len(buf) {
+			break
+		}
+	}
+	return nil
+}
+
+//Start start
+func (c *TCPNetwork) Start() {
+	fmt.Println("tcp run on localhost:7123")
+	listener, err := net.Listen("tcp", ":7123")
+	checkError(err)
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("break 1")
+
+				fmt.Println(err.Error())
+				break
+			}
+			go handleClient(conn)
+		}
+	}()
+	select {}
+
+}
+
+func handleClient(conn net.Conn) {
+	// close connection on exit
+	defer conn.Close()
+	var oneRead innerBuffer
+	var e error
+	for {
+		oneRead, e = readOnce(conn)
+		if e != nil {
+			if e == io.EOF {
+				break
+			}
+			fmt.Println(errorx.Wrap(e).Error())
+			return
+		}
+		buf, err := oneRead.readN(2)
 		if err != nil {
-			fmt.Println(err)
-			continue
+			fmt.Println("socket error:", err)
 		}
 
-		//处理逻辑
-		go handle(conn)
+		fmt.Println("receive from client:", binary.BigEndian.Uint16(buf))
+		fmt.Println(fmt.Sprintf("receive from client: %v", string(oneRead)))
+
+		// _, err2 := conn.Write(NewByte(1, 2, 3, 4, 5, 6, 7, 8, 9))
+
+		// if err2 != nil {
+		// 	fmt.Println(err2.Error())
+		// 	return
+		// }
 	}
 }
 
-func handle(conn net.Conn) {
-	//defer conn.Close()
-	fmt.Println("socket handle")
-	//读取客户端传送的消息
-	go func() {
-		var (
-			response innerBuffer
-			header   []byte
-			// err    error
-			// i      int
-		)
-		// response, _ = ioutil.ReadAll(conn)
-		// fmt.Println(string(response))
-
-		_, err := conn.Read(response)
-		header, err = response.readN(2)
-		fmt.Println("readN:", string(header), "-------err:", err)
-
-	}()
-
-	// //向客户端发送消息
-	// time.Sleep(1 * time.Second)
-	// now := time.Now().String()
-	// conn.Write([]byte(now))
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
 }
+
+func readOnce(reader io.Reader) ([]byte, error) {
+	var buffer = make([]byte, 512, 512)
+	var n int
+	var e error
+
+	n, e = reader.Read(buffer)
+	if e != nil {
+
+		return nil, e
+	}
+
+	return buffer[0:n], nil
+}
+
+// func NewByte(byts ...byte) []byte {
+// 	var rs = make([]byte, 0, 512)
+// 	rs = append(rs, byts...)
+// 	return rs
+// }
 
 //Stop NetworkInterface.Stop
 func (c *TCPNetwork) Stop() {
