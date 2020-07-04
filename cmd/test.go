0341acc1d5efae053fba4f249911cc409d4d8a22
wqdotnet <110328722@qq.com>
2020-07-04 09:34:00 +0800

    更新依赖

--- cmd/test.go
+++ cmd/test.go
@@ -16,6 +16,11 @@ limitations under the License.
 package cmd
 
 import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+
 	"github.com/spf13/cobra"
 )
 
@@ -25,7 +30,7 @@ var testCmd = &cobra.Command{
 	Short: "A brief description of your command",
 	Long:  `test`,
 	Run: func(cmd *cobra.Command, args []string) {
-
+		start()
 	},
 }
 
@@ -41,4 +46,46 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// testCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func start() {
+	ctx, cancel := context.WithCancel(context.Background())
+	eatNum := chiHanBao(ctx)
+	for n := range eatNum {
+		if n >= 10 {
+			cancel()
+			break
+		}
+	}
+
+	fmt.Println("正在统计结果。。。")
+	time.Sleep(1 * time.Second)
+}
+
+func chiHanBao(ctx context.Context) <-chan int {
+	c := make(chan int)
+	// 个数
+	n := 0
+	// 时间
+	t := 0
+	go func() {
+		for {
+			//time.Sleep(time.Second)
+			select {
+			case <-ctx.Done():
+				fmt.Printf("耗时 %d 秒，吃了 %d 个汉堡 \n", t, n)
+				return
+			case c <- n:
+				incr := rand.Intn(5)
+				n += incr
+				if n >= 10 {
+					n = 10
+				}
+				t++
+				fmt.Printf("我吃了 %d 个汉堡\n", n)
+			}
+		}
+	}()
+
+	return c
 }
